6551988a4a75f60459f9aecc7f7515cebe01e927
Jimmie Hogklint <hogklint@gmail.com>
2021-10-09 18:22:45 +0000

    Clone repos before configuration

--- manifests/crown.pp
+++ manifests/crown.pp
@@ -1,8 +1,8 @@
 node default {
   class {'packages': }
   class {'create_user': username => 'jhogklint', }
-  class {'system_config': username => 'jhogklint', }
   class {'user_repos': username => 'jhogklint', }
+  class {'system_config': username => 'jhogklint', }
   class {'user_config':
     username => 'jhogklint',
     host => 'crown',
