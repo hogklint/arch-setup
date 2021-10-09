2e9cd55e64e1f8584c7cac4cfd685cd18cc2424a
Jimmie Hogklint <hogklint@gmail.com>
2021-10-09 10:03:30 +0000

    Add create_user parameter

--- manifests/crown.pp
+++ manifests/crown.pp
@@ -1,6 +1,6 @@
 node default {
   class {'packages': }
-  class {'create_user': }
+  class {'create_user': username => 'jhogklint', }
   class {'system_config': username => 'jhogklint', }
   class {'user_repos': username => 'jhogklint', }
   class {'user_config':
