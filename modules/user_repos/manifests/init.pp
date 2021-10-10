b6ed1a04b0613f4633a1c0969639724c3fd86a7b
Jimmie Högklint <hogklint@gmail.com>
2021-10-10 14:07:34 +0200

    Add ssh origin to private repos

--- modules/user_repos/manifests/init.pp
+++ modules/user_repos/manifests/init.pp
@@ -12,7 +12,11 @@ class user_repos ($username){
   vcsrepo {"/home/$username/.vim":
     ensure => present,
     provider => git,
-    source => 'https://github.com/nomme/vim-env.git',
+    remote => 'https',
+    source => {
+      'origin' => 'privategithub:nomme/vim-env.git',
+      'https' => 'https://github.com/nomme/vim-env.git',
+    },
     user => "$username",
   }
 
@@ -37,21 +41,33 @@ class user_repos ($username){
   vcsrepo {"/home/$username/.zsh":
     ensure => present,
     provider => git,
-    source => 'https://github.com/nomme/zsh-env.git',
+    remote => 'https',
+    source => {
+      'origin' => 'privategithub:nomme/zsh-env.git',
+      'https' => 'https://github.com/nomme/zsh-env.git',
+    },
     user => "$username",
   }
 
   vcsrepo {"/home/$username/repos/user-files":
     ensure => present,
     provider => git,
-    source => 'https://github.com/nomme/user-files.git',
+    remote => 'https',
+    source => {
+      'origin' => 'privategithub:nomme/user-files.git',
+      'https' => 'https://github.com/nomme/user-files.git',
+    },
     user => "$username",
   }
 
   vcsrepo {"/home/$username/repos/arch-setup":
     ensure => present,
     provider => git,
-    source => 'https://github.com/nomme/arch-setup.git',
+    remote => 'https',
+    source => {
+      'origin' => 'privategithub:nomme/arch-setup.git',
+      'https' => 'https://github.com/nomme/arch-setup.git',
+    },
     user => "$username",
   }
 
